3e3081b2cb272a080f3c8a6d9b1733a594e43325
Anirudh Phukan <anirudh.phukan@gmail.com>
2021-03-15 21:06:15 +0530

    Final changes made to DeletePageImpl

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -95,7 +95,7 @@ bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) {
     }
     return false;
   }
-  return false;
+  return true;
 }
 
 bool BufferPoolManager::FlushPageImpl(page_id_t page_id) {
@@ -152,34 +152,35 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
   return &pages_[free_frame_id];
 }
 
-// NOTE: Assumes user calls flush to propagate any unsaved changes. This function just destroys and removes a page.
 bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
-  // 0.   Make sure you call DiskManager::DeallocatePage!
+  std::lock_guard<std::mutex> guard(latch_);
+  
   // 1.   Search the page table for the requested page (P).
   // 1.   If P does not exist, return true.
-  // TODO: think about whether we should add lock
   if (page_table_.find(page_id) == page_table_.end()) {
     disk_manager_->DeallocatePage(page_id);
     return true;
   }
-  frame_id_t frame_id = page_table_[page_id];
-  Page *pageToDelete = &pages_[frame_id];
-
-  // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
-  if (pageToDelete->GetPinCount() > 0) {
+  
+  Page *p = &pages_[page_table_[page_id]];
+  
+  // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.	
+  if (p->pin_count_ > 0) {
     return false;
   }
-
-  // 3.   Otherwise, P can be deleted.
-  // Remove P from the page table, reset its metadata and return it to the free list.
+  
+   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
+  page_table_.erase(p->page_id_); // Removing P from the page table
+  
+  // Reseting metadata
+  p->ResetMemory();
+  p->page_id_ = INVALID_PAGE_ID;
+  p->is_dirty_ = false;
+  p->pin_count_ = 0;
+  
+  // Returning back to the free list
   free_list_.emplace_back(page_table_[page_id]);
-  page_table_.erase(pageToDelete->page_id_);
-  pageToDelete->ResetMemory();
-  // TODO refactor these lines to a function ResetPage()
-  pageToDelete->page_id_ = INVALID_PAGE_ID;
-  pageToDelete->is_dirty_ = false;
-  pageToDelete->pin_count_ = 0;
-  return false;
+  return true;
 }
 
 void BufferPoolManager::FlushAllPagesImpl() {
